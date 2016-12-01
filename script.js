68ab2433abaaa1a054d1072c06583d7181ced40b
Jonathan Reiser <jdreiser1@gmail.com>
2016-12-01 18:20:09 -0500

    Initial Commit

---
+++ script.js
@@ -0,0 +1,10 @@
+window.addEventListener("deviceorientation", handleOrientation, true)
+
+function handleOrientation(event){
+  var absolute = event.absolute;
+  var alpha = event.alpha;
+  var beta = event.beta;
+  var gamma = event.gamma;
+
+  document.getElementById("alpha").innerHTML = alpha;
+}
